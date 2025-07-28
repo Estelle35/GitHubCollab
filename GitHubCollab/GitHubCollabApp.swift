812e3bbfc8b50aff0de51f4f9a34e0fceee92b63
Estelle Joubert <estellejoubert067@gmail.com>
2025-07-28 09:54:07 -0400

    Initial Commit

---
+++ GitHubCollab/GitHubCollabApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubCollabApp.swift
+//  GitHubCollab
+//
+//  Created by Scholar on 7/28/25.
+//
+
+import SwiftUI
+
+@main
+struct GitHubCollabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
